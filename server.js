3f6b0f06cd1fa1e3c66e1428b5ff33417b0effb7
DishaNayak <dishanyk@gmail.com>
2018-03-02 00:43:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,17 +5,68 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleOne={
+    title:'Article One|Disha Nayak|',
+    heading:'Article One',
+    date:'Feb 26,2018',
+    content:`<p>
+                    Welcome to my first article,Here I begin my learning journey at IMAD. 
+                </p>
+                <p>
+                    It is simple and easy to learn.
+                </p>
+                <p>
+                    Why don't you join with me ?
+                </p>
+}
+function createTemplate (data){
+var title=data.title;
+var heading=data.heading;
+var date= data.date;
+var content=data.content;
+
+var htmlTemplate=
+<html>
+    <head>
+        <title>
+            $(title)
+        </title>
+        <meta name="viewport" content="width=devicewidth,initial-scale=1"/>
+         <link href="/ui/style.css" rel="stylesheet" />
+        <body>
+            <div class= "container">
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <hr/>
+            <h3>
+               $(heading)
+            </h3>
+            <div>
+               $(date)
+            </div>
+            <div>
+               $(content)
+            </div>
+            </div>
+        </body>
+</html>
+';
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createTemplate(articleOne));
 });   
 
 app.get('/article-two', function (req, res) {
      res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
+});       
 
 app.get('/article-three', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
