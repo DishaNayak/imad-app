3b7c2b2c79dd1e99af04c571947db837bb3e8b4f
DishaNayak <dishanyk@gmail.com>
2018-03-07 12:54:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,19 @@
 console.log('Loaded!');
 var button=document.getElementById('counter');
-var counter=0;
 
-button.onclick=function(){
-counter=counter+1;
-var span=document.getElementById('count');
-span.innerHTML=counter.toString();
+button.onclick= function() {
+    var request=new XMLHttpReuest();
+        request.onreadystatechange = function(){
+            if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200){
+              var counter= request.responseText;
+              var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
+    }
+        }
+        
+    };
+    request.open('GET','http://dishanyk.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
+    
